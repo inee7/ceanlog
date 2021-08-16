0637192b7a08dac95a09f253a82f0f74acfea213
inee7 <jongin012@gmail.com>
2021-08-17 01:28:01 +0900

    init

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Bee starter`,
+  description: `Blog posted about ...`,
+  author: `ocean`,
+  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  siteUrl: ``, // Your blog site url
+  social: {
+    github: `inee7.github.com`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
